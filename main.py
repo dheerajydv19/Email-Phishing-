d3a436ea36a19ec03d20408d949432d7117e595f
Dheeraj Yadav <104244227+dheerajydv19@users.noreply.github.com>
2023-01-03 20:44:06 +0530

    Added the module analyzing headers

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # Phishing Email Analysis
 import email
 import email_headers
+import analyzing_headers
 
 
 
@@ -9,4 +10,4 @@ import email_headers
 # Usage
 eml_file = "test.eml"
 email_headers.print_email_headers(eml_file)
-
+analyzing_headers.analyze_email_headers(eml_file)
